46b1c3e4f9b2044958ab6efc91af3089bdc4eecf
alvaDenBasta <alvachristensson03@gmail.com>
2026-05-06 15:52:33 +0200

    continued to work on the problem

--- WS.py
+++ WS.py
@@ -1,4 +1,33 @@
 import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+#number of nodes
+N = 10
+#degree of node
+k = 4
+#probability
+#p = [0,0.001,0.003,0.01,0.1,0.3,1]
+p = 0.1
+
 G = nx.Graph()
 
-#testar saker
+for i in range(N):
+    G.add_node(i)
+
+r = int(k/2)
+for i in range(N):
+    for j in range(1,r+1):
+        if (np.random.random()<p):
+            index = (i+np.random.randint(1,20))%N
+            G.add_edge(i,index)
+        elif(i+j<0):
+            G.add_edge(i,N+i+j)
+        elif(i+j>N-1):
+            G.add_edge(i,i+j-N)
+        else:
+            G.add_edge(i,j+i)
+
+# Circular Layout
+nx.draw_circular(G, with_labels=True)
+plt.savefig("filename.png")
