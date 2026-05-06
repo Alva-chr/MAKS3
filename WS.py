ccb71a10b5f1b24be522fe71c415864d91392642
alvaDenBasta <alvachristensson03@gmail.com>
2026-05-06 16:41:07 +0200

    saker funkar inte

--- WS.py
+++ WS.py
@@ -10,6 +10,8 @@ k = 4
 #p = [0,0.001,0.003,0.01,0.1,0.3,1]
 p = 0.1
 
+
+
 G = nx.Graph()
 
 for i in range(N):
@@ -18,16 +20,25 @@ for i in range(N):
 r = int(k/2)
 for i in range(N):
     for j in range(1,r+1):
-        if (np.random.random()<p):
-            index = (i+np.random.randint(1,20))%N
-            G.add_edge(i,index)
-        elif(i+j<0):
+        if(i+j<0):
             G.add_edge(i,N+i+j)
         elif(i+j>N-1):
             G.add_edge(i,i+j-N)
         else:
             G.add_edge(i,j+i)
 
+
+nx.random_reference(G, p, False, None)
+
+# existing_edges = list(G.edges)
+
+# for i in existing_edges:
+#     if (np.random.random()<p):
+#         index = (i+np.random.randint(1,N))%N
+#         if(index)
+#         G.add_edge(index)
+
+
 # Circular Layout
 nx.draw_circular(G, with_labels=True)
-plt.savefig("filename.png")
+plt.savefig("outputq.png")
